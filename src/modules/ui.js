db20f7e40eb65b8a3a6f63afac8fac0115a675c8
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-02-18 17:35:56 +0000

    feat: update task form layout and input fields for better user experience

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -4,14 +4,19 @@ export const UI = {
         if (!root) return; // Ila l-root makhdamch, mat-dir walou
 
         root.innerHTML = `
-            <div class="task-form-container">
-                <h2>Create New Task</h2>
-                <div class="input-group">
-                    <label>TASK TITLE</label>
-                    <input type="text" id="task-name" placeholder="Enter task name...">
-                </div>
-                <button class="btn btn--teal" id="submit-task">Add Task ></button>
-            </div>
+         <input type="text" id="title" placeholder="Entrer tasks name" />
+    <p id="titleError" class="error-message"></p>
+    <label for="urgency">Urgency (1-5):</label>
+<input type="number" id="urgency" min="1" max="5" />
+
+<label for="importance">Importance (1-5):</label>
+<input type="number" id="importance" min="1" max="5" />
+
+<label for="effort">Effort (1-5):</label>
+<input type="number" id="effort" min="1" max="5" />
+
+<button id="addTaskBtn">Add Task</button>
+<p id="formError" class="error-message"></p>
         `;
     }
 };
