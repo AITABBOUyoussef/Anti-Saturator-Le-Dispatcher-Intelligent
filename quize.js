89cb372cc962f9132c5bb81fecd283592985e430
jihanejador <jihanejador@gmail.com>
2026-02-18 11:16:58 +0000

    creation dom et la fonction

--- quize.js
+++ quize.js
@@ -1,5 +1,5 @@
 const Good = document.getElementById("good");
-const notgood = document.getElementById("not good");
+const notgood = document.getElementById("notgood");
 const  bad = document.getElementById("bad");
 const happy =document.getElementById("happy");
 const ok = document.getElementById("ok");
@@ -7,5 +7,18 @@ const sad = document.getElementById("sad");
 const very = document.getElementById("very");
 const normal = document.getElementById("normal");
 const No = document.getElementById("no");
+
+let totalscore = 0;
+let answerd = 0;
+
+function handleAnsawer(value){
+    totalscore+=value;
+    answerd++;
+    if(answerd===3){
+      const energy = Math.round(totalscore/3);
+      alert("your energy level is : "+ energy);
+    }
+}
+
 
 
