8c7ed6eb9e6cb87c6b1a6364ca8fdda417f10f31
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-02-18 17:33:32 +0000

    fix: correct button selector for task addition functionality

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import { UI } from './modules/ui.js';
 
 // 1. Qbed l-bouton mn l-Sidebar
-const addTaskBtn = document.querySelector('.btn--task');
+const addTaskBtn = document.querySelector('.btn--secondary');
 
 // 2. Event Listener: Mnin l-user i-cliki, t-ban l-form
 addTaskBtn.addEventListener('click', () => {
