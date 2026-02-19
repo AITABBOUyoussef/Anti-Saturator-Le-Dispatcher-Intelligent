8714d86c784f338a3427adf688a835d3dd051740
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-02-19 10:01:53 +0000

    feat: implement task management features with validation and mock data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 import { UI } from './modules/ui.js';
 
+import { getTasks, saveTasks } from "./modules/storage.js";
+import { validateTitle, validateScore } from "./modules/validator.js";
+import { addtaskkk } from './modules/todo.js';
 // 1. Qbed l-bouton mn l-Sidebar
 const addTaskBtn = document.querySelector('.btn--secondary');
 
 // 2. Event Listener: Mnin l-user i-cliki, t-ban l-form
 addTaskBtn.addEventListener('click', () => {
     UI.renderAddTaskForm();
+    let tasks = getTasks();
+  console.log("Tasks:", tasks);
+
+  
+  const titleInput = document.getElementById("title");
+  const urgencyInput = document.getElementById("urgency");
+  const importanceInput = document.getElementById("importance");
+  const effortInput = document.getElementById("effort");
+  const addBtn = document.getElementById("addTaskBtn");
+  const formError = document.getElementById("formError");
+
+  
+  addBtn.addEventListener("click", () => {
+   console.log("rgefzdas");
+    addtaskkk(titleInput,urgencyInput,importanceInput,effortInput,formError,tasks)
+  })
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  
 });
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,22 +1,24 @@
+
 export const UI = {
     renderAddTaskForm: () => {
         const root = document.getElementById('root'); 
-        if (!root) return; // Ila l-root makhdamch, mat-dir walou
+        if (!root) return;
 
+        // 1. Rsem l-HTML dial l-formulaire
         root.innerHTML = `
-         <input type="text" id="title" placeholder="Entrer tasks name" />
-    <p id="titleError" class="error-message"></p>
-    <label for="urgency">Urgency (1-5):</label>
-<input type="number" id="urgency" min="1" max="5" />
-
-<label for="importance">Importance (1-5):</label>
-<input type="number" id="importance" min="1" max="5" />
-
-<label for="effort">Effort (1-5):</label>
-<input type="number" id="effort" min="1" max="5" />
-
-<button id="addTaskBtn">Add Task</button>
-<p id="formError" class="error-message"></p>
+            <input type="text" id="title" placeholder="Entrer tasks name" />
+            <p id="titleError" class="error-message"></p>
+            <label for="urgency">Urgency (1-5):</label>
+            <input type="number" id="urgency" min="1" max="5" />
+            <label for="importance">Importance (1-5):</label>
+            <input type="number" id="importance" min="1" max="5" />
+            <label for="effort">Effort (1-5):</label>
+            <input type="number" id="effort" min="1" max="5" />
+            <button id="addTaskBtn">Add Task</button>
+            <p id="formError" class="error-message"></p>
         `;
+
+        
     }
+    
 };
