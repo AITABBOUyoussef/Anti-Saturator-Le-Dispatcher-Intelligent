57d3e9330a62578a09ef113c93b0bb152171d6e1
jihanejador <jihanejador@gmail.com>
2026-02-19 10:01:14 +0000

    creation les functions de calculer energy

--- quize.js
+++ quize.js
@@ -1,6 +1,24 @@
-let totalscore=0;
-function nextQuestion(value, nextId){
-  totalscore+=value;
-  document.getElementById(nextId).style.display="block";
+let totalScore = 0;
 
+document.getElementById("good").onclick = () => nextQuestion(8, "q2");
+document.getElementById("notgood").onclick = () => nextQuestion(5, "q2");
+document.getElementById("bad").onclick = () => nextQuestion(2, "q2");
+
+document.getElementById("happy").onclick = () => nextQuestion(9, "q3");
+document.getElementById("ok").onclick = () => nextQuestion(5, "q3");
+document.getElementById("sad").onclick = () => nextQuestion(2, "q3");
+
+document.getElementById("very").onclick = () => finishQuiz(9);
+document.getElementById("normal").onclick = () => finishQuiz(5);
+document.getElementById("no").onclick = () => finishQuiz(1);
+
+function nextQuestion(value, nextId) {
+    totalScore += value;
+    document.getElementById(nextId).style.display = "block";
+}
+
+function finishQuiz(value) {
+    totalScore += value;
+    const energy = Math.round(totalScore / 3);
+    alert("Your energy level is: " + energy);
 }
